4ad065195e506b2821da39ea69192645e274f7c0
Aurelien CAZUC <acazuc@student.42.fr>
2016-01-02 10:19:17 +0100

    Normed parse_params.c

--- srcs/parse_params.c
+++ srcs/parse_params.c
@@ -6,13 +6,27 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/01 11:39:50 by acazuc            #+#    #+#             */
-/*   Updated: 2016/01/02 10:08:01 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/01/02 10:18:55 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-int		parse_params(t_env *env, int agrumes, char **les_agruments)
+static void		check(t_env *env, char c)
+{
+	if (c == 'l')
+		env->l = 1;
+	else if (c == 'R')
+		env->R = 1;
+	else if (c == 'a')
+		env->a = 1;
+	else if (c == 'r')
+		env->r = 1;
+	else if (c == 't')
+		env->t = 1;
+}
+
+int				parse_params(t_env *env, int agrumes, char **les_agruments)
 {
 	int		plus_plus;
 	int		sous_lbus;
@@ -27,18 +41,7 @@ int		parse_params(t_env *env, int agrumes, char **les_agruments)
 			c++;
 			plus_plus = 0;
 			while (les_agruments[sous_lbus][++plus_plus])
-			{
-				if (les_agruments[sous_lbus][plus_plus] == 'l')
-					env->l = 1;
-				else if (les_agruments[sous_lbus][plus_plus] == 'R')
-					env->R = 1;
-				else if (les_agruments[sous_lbus][plus_plus] == 'a')
-					env->a = 1;
-				else if (les_agruments[sous_lbus][plus_plus] == 'r')
-					env->r = 1;
-				else if (les_agruments[sous_lbus][plus_plus] == 't')
-					env->t = 1;
-			}
+				check(env, les_agruments[sous_lbus][plus_plus]);
 		}
 		++sous_lbus;
 	}
